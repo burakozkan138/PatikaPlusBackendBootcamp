06d33687ebd8d8016c6431719b2183295cb72efb
burakozkan138 <72349282+burakozkan138@users.noreply.github.com>
2024-09-30 09:15:41 +0300

    Initial commit

---
+++ Week-1/FirstProject/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(@"Burak Özkan.
+Patika Bootcamp'e katılma nedenim, yazılım ve web geliştirme alanında kendimi geliştirmek, yeni teknolojileri öğrenmek ve profesyonel hayatımda daha yetkin bir geliştirici olmak. 
+Bu bootcamp, kariyer hedeflerime ulaşmam için bana önemli fırsatlar sunuyor ve bu alandaki bilgi ve becerilerimi artırma şansı veriyor.");
