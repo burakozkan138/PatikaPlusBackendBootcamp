45a51ec9c867b98b1599c50395e596f05894affa
burakozkan138 <72349282+burakozkan138@users.noreply.github.com>
2024-10-01 13:21:43 +0300

    initilaze variable demo project

---
+++ Week-1/VariablesDemo/Program.cs
@@ -0,0 +1,31 @@
+string? tc, name, surname, phone;
+// tc and phone number can be null so we use nullable string type
+
+int age;
+double firstProductPrice, secondProductPrice, totalPrice;
+
+Console.Clear();
+Console.WriteLine("Variables Demo Uygulamasına Hoşgeldiniz");
+Console.Write("Lütfen T.C Kimlik Numaranızı Giriniz: ");
+tc = Console.ReadLine(); // return string or null
+Console.Write("Lütfen Adınızı Giriniz: ");
+name = Console.ReadLine();
+Console.Write("Lütfen Soyadınızı Giriniz: ");
+surname = Console.ReadLine();
+Console.Write("Lütfen Telefon Numaranızı Giriniz: ");
+phone = Console.ReadLine(); // if you want to store phone number as integer, you can use int.Parse(Console.ReadLine()); instead but not start with 0
+Console.Write("Lütfen Yaşınızı Giriniz: ");
+age = Convert.ToInt32(Console.ReadLine());
+Console.Write("Lütfen İlk Aldığınız Ürünün Fiyatını Giriniz: ");
+firstProductPrice = Convert.ToDouble(Console.ReadLine());
+Console.Write("Lütfen İkinci Aldığınız Ürünün Fiyatını Giriniz: ");
+secondProductPrice = Convert.ToDouble(Console.ReadLine());
+
+totalPrice = firstProductPrice + secondProductPrice;
+
+Console.WriteLine($"\n{tc} Kimlik numaralı, {name} {surname} adlı kişi için kayıt alınmıştır.");
+Console.WriteLine($"{phone} Telefon numarasına bildirim mesajı gönderilmiştir.");
+Console.WriteLine($"{totalPrice} TL tutarındaki alışverişiniz için teşekkür ederiz kazanılan 10% puan miktarı -> {(totalPrice * 0.1) + (age * 0.1)} TL");
+
+Console.ReadLine();
+Console.WriteLine("\nProgram Sonlandı. Çıkmak için bir tuşa basınız.");
