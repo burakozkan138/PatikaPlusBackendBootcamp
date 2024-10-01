b69b69a51efb5b4fed31255be169e16ba4f5f9b7
burakozkan138 <72349282+burakozkan138@users.noreply.github.com>
2024-10-01 13:42:51 +0300

    initilaze ConditionalDemo

---
+++ Week-1/ConditionalDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Lütfen bir sayı giriniz: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 10)
+{
+  Console.WriteLine("Girilen sayı 10'a eşittir.");
+}
+else if (number < 10)
+{
+  Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+else
+{
+  Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+}
