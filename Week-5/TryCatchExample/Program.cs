56848a1116078fcd8b7a01bb6313bd5d7756bd9f
burakozkan138 <72349282+burakozkan138@users.noreply.github.com>
2024-10-21 22:50:06 +0300

    init

---
+++ Week-5/TryCatchExample/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+try
+{
+  Console.Write("Enter a number: ");
+  int number = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("The number squared is: " + number * number);
+}
+catch (FormatException)
+{
+  Console.WriteLine("Invalid input. Please enter a number.");
+}
+catch (Exception ex)
+{
+  Console.WriteLine("An error occurred: " + ex.Message);
+}
+finally
+{
+  Console.WriteLine("Program complete.");
+}
+
+Console.Write("Press any key to exit...");
+Console.ReadKey();
